b7e729bea24d706e66631cf2c84d08de74a4adc0
unknown <tsdcanavan@yahoo.com>
2017-11-11 12:20:24 -0500

    commit hero_scroll to public folder

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
+
 INSERT INTO categorytbls
 (primarycat, secondarycat)
 values ('Mens',''), ('Ladies',''),
 ('Kids',''), ('Mens', 'Shirts'), ('Mens', 'Pants'),
 ('Mens', 'Shoes'), ('Ladies','Dresses'), ('Ladies', 'Shirts'),
 ('Ladies','Pants'), ('Ladies','Shoes'), ('Kids','Shirts'),
-('Kids','Pants'),('Kids','Shoes')
-
+('Kids','Pants'),('Kids','Shoes');
