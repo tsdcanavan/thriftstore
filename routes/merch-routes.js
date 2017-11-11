d603f12fe1998bfe394a735bf980e97845e21621
Albert Hong <alby.hong@gmail.com>
2017-11-11 11:28:31 -0500

    albert's push

---
+++ routes/merch-routes.js
@@ -0,0 +1,72 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+
+
+	app.get("/api/merch", function(req, res) {
+		var query = {};
+		if (req.query.merch_id) {
+			query.id = req.query.merch_id;
+		}
+
+		db.Merchtbls.findAll({
+			include: [db.Merchtbls],
+			where: query
+		}).then(function(dbMerch) {
+			res.json(dbMerch);
+		});
+	});
+
+
+
+	app.get("/api/posts/:id", function(req, res) {
+   
+	    db.Merch.findOne({
+	      where: {
+	        id: req.params.id
+	      },
+	      include: [db.Usertbls]
+	    }).then(function(dbMerch) {
+	      console.log(dbMerch);
+	      res.json(dbMerch);
+	    });
+	  });
+
+
+
+
+	 app.post("/api/merch", function(req, res) {
+	    db.Merchtbls.create(req.body).then(function(dbMerch) {
+	      res.json(dbMerch);
+	    });
+	  });
+
+
+	app.delete("/api/Merch/:id", function(req, res) {
+	    db.Merchtbls.destroy({
+	      where: {
+	        id: req.params.id
+	      }
+	    }).then(function(dbMerch) {
+	      res.json(dbMerch);
+	    });
+	  });
+
+  // PUT route for updating merch
+  	app.put("/api/Merch", function(req, res) {
+	    db.Merchtbls.update(
+	      req.body,
+	      {
+	        where: {
+	          id: req.body.id
+	        }
+	      }).then(function(dbMerch) {
+	        res.json(dbMerch);
+	      });
+	  });
+
+
+
+
+}
